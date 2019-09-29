10d69a0cf4981afcda80bea46109b6fc8e0e7fbd
Jamie Hale <jamiesonhale@gmail.com>
2019-09-29 14:16:11 -0400

    Add in-memory caching

--- src/lib/repository.js
+++ src/lib/repository.js
@@ -1,86 +1,53 @@
 import axios from 'axios';
 import * as R from 'ramda';
 
-const buildBundle = (version, dependencies = {}) => ({
-  version,
-  dependencies,
-});
+const depVersion = (dep, version) => `${dep}@${version}`;
 
-const buildBundles = (name, bundleDefs) => ({
-  [name]: {
-    manifest: {
-      name,
-      latest: bundleDefs[bundleDefs.length - 1].version,
-      versions: bundleDefs.map(def => def.version),
-    },
-    versions: {
-      ...bundleDefs.reduce((acc, bundleDef) => ({
-        ...acc,
-        [bundleDef.version]: {
-          name,
-          ...bundleDef,
-        },
-      }), {}),
-    },
-  },
-});
+const manifestCache = {};
+const descriptorCache = {};
 
-const repo = {
-  ...buildBundles(
-    'dnd4e',
-    [
-      buildBundle('1.0.0', { a: '^1.0.0', b: '^1.0.0' }),
-      buildBundle('1.0.1', { a: '^1.0.1', b: '^1.0.0' }),
-      buildBundle('1.0.2', { a: '^1.0.1', b: '^1.0.0' }),
-    ],
-  ),
-  ...buildBundles(
-    'a',
-    [
-      buildBundle('1.0.0', { c: '^1.0.0' }),
-      buildBundle('1.0.1', { c: '^1.0.1' }),
-      buildBundle('1.0.2', { c: '^1.0.1' }),
-      buildBundle('1.1.0', { c: '^1.0.1' }),
-      buildBundle('2.0.0', { c: '^1.0.1' }),
-    ],
-  ),
-  ...buildBundles(
-    'b',
-    [
-      buildBundle('1.0.0', { c: '^1.0.0' }),
-    ],
-  ),
-  ...buildBundles(
-    'c',
-    [
-      buildBundle('1.0.0', {}),
-      buildBundle('1.0.1', {}),
-    ],
-  ),
-  ...buildBundles(
-    'classes',
-    [
-      buildBundle('1.0.0', { stuff: '^1.0.0' }),
-    ],
-  ),
-  ...buildBundles(
-    'stuff',
-    [
-      buildBundle('1.0.0', {}),
-    ],
-  ),
+const cacheManifest = async (dep, manifest) => {
+  manifestCache[dep] = manifest;
 };
 
+const cacheBundleDescriptor = async (depVersion, bundleDescriptor) => {
+  descriptorCache[depVersion] = bundleDescriptor;
+};
+
+const getCachedManifest = async dep => manifestCache[dep];
+
+const getCachedBundleDescriptor = async depVersion => descriptorCache[depVersion];
+
 export const loadManifestFor = async dep => {
+  const cachedManifest = await getCachedManifest(dep);
+  if (cachedManifest) {
+    console.log(`Using cached bundle manifest for ${dep}.`);
+    return cachedManifest;
+  }
+
   console.log(`Fetching bundle manifest for ${dep}...`);
-  return axios.get(`http://localhost:8080/api/v1/bundles/${dep}/manifest`)
+  const freshManifest = await axios.get(`http://localhost:8080/api/v1/bundles/${dep}/manifest`)
     .then(R.prop('data'));
+
+  console.log(`Caching bundle manifest for ${dep}...`);
+  await cacheManifest(dep, freshManifest);
+  return freshManifest;
 };
 
 export const loadBundleDescriptorFor = async (dep, version) => {
-  console.log(`Fetching bundle descriptor for ${dep}@${version}...`);
-  return axios.get(`http://localhost:8080/api/v1/bundles/${dep}/releases/${version}`)
+  const cachedBundleDescriptor = await getCachedBundleDescriptor(depVersion(dep, version));
+  if (cachedBundleDescriptor) {
+    console.log(`Using cached bundle descriptor for ${depVersion(dep, version)}.`)
+    return cachedBundleDescriptor;
+  }
+
+  console.log(`Fetching bundle descriptor for ${depVersion(dep, version)}...`);
+  const freshBundleDescriptor = await axios.get(`http://localhost:8080/api/v1/bundles/${dep}/releases/${version}`)
     .then(R.prop('data'));
+  
+  console.log(`Caching bundle descriptor for ${depVersion(dep, version)}...`);
+  await cacheBundleDescriptor(depVersion(dep, version), freshBundleDescriptor);
+  return freshBundleDescriptor;
 };
 
 export const getLatestVersionFor = async dep => {
