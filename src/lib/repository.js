76f0ab873e79e9e2d56ba7349d4cb73b8eb2436a
Jamie Hale <jamiesonhale@gmail.com>
2019-09-29 17:21:13 -0400

    Streaming manifests

--- src/lib/repository.js
+++ src/lib/repository.js
@@ -4,81 +4,50 @@ import fs from 'fs';
 import os from 'os';
 import mkdirp from 'mkdirp';
 import recursiveCopy from 'recursive-copy';
+import tar from 'tar';
+import { writer } from 'repl';
 
 const cachePath = () => {
   const homeDir = os.homedir();
   return `${homeDir}/.tbm/cache`;
 };
 
-const initializeConfig = () => {
+const bundleCachePath = name => `${cachePath()}/${name}`;
+
+const tarballFilename = (name, version) => `${name}-${version}.tgz`;
+
+const ensureCachePathExists = () => {
   mkdirp.sync(cachePath());
+};
+
+const ensureBundleCachePathExists = (name) => {
+  mkdirp.sync(bundleCachePath(name));
+};
+
+const initializeConfig = () => {
+  ensureCachePathExists();
 };  
 
 const depVersion = (dep, version) => `${dep}@${version}`;
 
 const manifestCache = {};
-const descriptorCache = {};
 
 const cacheManifest = async (dep, manifest) => {
   manifestCache[dep] = manifest;
 };
 
-const cacheBundleDescriptor = async (depVersion, bundleDescriptor) => {
-  descriptorCache[depVersion] = bundleDescriptor;
-};
-
 const getCachedManifest = async dep => manifestCache[dep];
 
-const getCachedBundleDescriptor = async depVersion => descriptorCache[depVersion];
-
-export const loadManifestFor = async dep => {
-  initializeConfig();
-  const cachedManifest = await getCachedManifest(dep);
-  if (cachedManifest) {
-    console.log(`Using cached bundle manifest for ${dep}.`);
-    return cachedManifest;
-  }
-
-  console.log(`Fetching bundle manifest for ${dep}...`);
-  const freshManifest = await axios.get(`http://localhost:8080/api/v1/bundles/${dep}/manifest`)
-    .then(R.prop('data'));
-
-  console.log(`Caching bundle manifest for ${dep}...`);
-  await cacheManifest(dep, freshManifest);
-  return freshManifest;
-};
-
-export const loadBundleDescriptorFor = async (dep, version) => {
-  initializeConfig();
-  const cachedBundleDescriptor = await getCachedBundleDescriptor(depVersion(dep, version));
-  if (cachedBundleDescriptor) {
-    console.log(`Using cached bundle descriptor for ${depVersion(dep, version)}.`)
-    return cachedBundleDescriptor;
-  }
-
-  console.log(`Fetching bundle descriptor for ${depVersion(dep, version)}...`);
-  const freshBundleDescriptor = await axios.get(`http://localhost:8080/api/v1/bundles/${dep}/releases/${version}`)
-    .then(R.prop('data'));
-  
-  console.log(`Caching bundle descriptor for ${depVersion(dep, version)}...`);
-  await cacheBundleDescriptor(depVersion(dep, version), freshBundleDescriptor);
-  return freshBundleDescriptor;
-};
-
-export const getLatestVersionFor = async dep => {
-  const manifest = await loadManifestFor(dep);
-  return manifest.latest;
-};
-
-const ensureBundleIsCached = async (name, version) => {
-  if (fs.existsSync(`${cachePath()}/${name}/${version}`)) {
+const ensureManifestIsCached = async (dep) => {
+  if (fs.existsSync(`${bundleCachePath(dep)}/manifest.json`)) {
     return;
   }
 
-  mkdirp.sync(`${cachePath()}/${name}/${version}`);
-  const writer = fs.createWriteStream(`${cachePath()}/${name}/${version}/bundle.json`);
-
-  await axios.get(`http://localhost:8080/api/v1/bundles/${name}/releases/${version}`, { responseType: 'stream' })
+  console.log(`Fetching bundle manifest for ${dep}...`);
+  ensureBundleCachePathExists(dep);
+  const manifestPath = `${bundleCachePath(dep)}/manifest.json`;
+  const writer = fs.createWriteStream(manifestPath);
+  await axios.get(`http://localhost:8080/api/v1/bundles/${dep}/manifest`, { responseType: 'stream' })
     .then(response => {
       response.data.pipe(writer);
       return new Promise((resolve, reject) => {
@@ -88,8 +57,48 @@ const ensureBundleIsCached = async (name, version) => {
     });
 };
 
+export const loadManifestFor = async dep => {
+  initializeConfig();
+  await ensureManifestIsCached(dep);
+
+  return JSON.parse(fs.readFileSync(`${bundleCachePath(dep)}/manifest.json`, 'utf-8'));
+};
+
+export const loadBundleDescriptorFor = async (dep, version) => {
+  initializeConfig();
+  await ensureBundleIsCached(dep, version);
+
+  return JSON.parse(fs.readFileSync(`${cachePath()}/${dep}/${dep}-${version}/bundle.json`, 'utf-8'));
+};
+
+export const getLatestVersionFor = async dep => {
+  const manifest = await loadManifestFor(dep);
+  return manifest.latest;
+};
+
+const ensureBundleIsCached = async (name, version) => {
+  if (fs.existsSync(`${cachePath()}/${name}/${name}-${version}/bundle.json`)) {
+    return;
+  }
+
+  console.log(`Fetching bundle for ${name}@${version}...`);
+  ensureBundleCachePathExists(name);
+  const tarballPath = `${bundleCachePath(name)}/${tarballFilename(name, version)}`;
+  const writer = fs.createWriteStream(tarballPath);
+  await axios.get(`http://localhost:8080/api/v1/bundles/${name}/releases/${version}`, { responseType: 'stream' })
+    .then(response => {
+      response.data.pipe(writer);
+      return new Promise((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+      });
+    });
+  
+  await tar.extract({ file: tarballPath, cwd: bundleCachePath(name) });
+};
+
 export const unpackBundleInto = async (name, version, path) => {
   initializeConfig();
   await ensureBundleIsCached(name, version);
-  await recursiveCopy(`${cachePath()}/${name}/${version}`, path);
+  await recursiveCopy(`${cachePath()}/${name}/${name}-${version}`, path);
 };
